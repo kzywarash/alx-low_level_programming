3bcf99d49ff86abd41b53266dea5f3ef4a05a012
kzywarash <wabongwe@gmail.com>
2023-02-17 13:46:56 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Main finction
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
