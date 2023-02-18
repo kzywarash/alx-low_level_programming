85188bbf79c5d7d522a270922065eb6544564d6a
kzywarash <wabongwe@gmail.com>
2023-02-17 16:03:24 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	a = 0;
+	b = 'a';
+	while (a < 10)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++
+	}
+	putchar('\n');
+	return (0);
+}
