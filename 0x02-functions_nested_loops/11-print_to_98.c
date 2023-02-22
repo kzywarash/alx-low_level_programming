afd51c38e1fed9cdf3a6b4faf106dccb37227e3b
kzywarash <wabongwe@gmail.com>
2023-02-21 18:46:52 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,18 +14,18 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				_putchar(i);
+				printf("%d, ", i);
 			else if (i == 98)
-				_putchar(i);
+				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				_putchar(j);
+				printf("%d, ", j);
 			else if (j == 98)
-				_putchar(j);
+				printf("%d\n", j);
 		}
 	}
 }
