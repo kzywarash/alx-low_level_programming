e83f4b83f76d3e7d2744bf643e2bc3e169e4e4b3
Abongwe Wara <wabongwe@gmail.com>
2023-06-16 18:44:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to print all single numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
