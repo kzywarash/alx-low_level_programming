7a5e66396777ab62ac7420158aec08dc4c0906f0
Abongwe Wara <wabongwe@gmail.com>
2023-06-20 15:47:34 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - program that prints _putchar, followed by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar(95); /*95 is ascii for _*/
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
