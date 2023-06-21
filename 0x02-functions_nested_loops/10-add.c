77a32c57e0b0864b1adb3339a85d61804aa5615d
Abongwe Wara <wabongwe@gmail.com>
2023-06-20 18:21:59 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * @a: integer to be added
+ * @b: integer to be added
+ * Return: sum of integers
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
