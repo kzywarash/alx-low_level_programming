e74f7e58cf92935dda098bea4040f1b44be8780e
Abongwe Wara <wabongwe@gmail.com>
2023-06-20 19:01:47 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,18 +9,18 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
-		printf("\n");
+		printf("98\n");
 	}
 	else
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
-		printf("\n");
+		printf("98\n");
 	}
 }
