d07ae5fa32770be8a91109f44d19bbb89a2591a7
Abongwe Wara <wabongwe@gmail.com>
2023-06-22 18:10:17 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints numbers from 1 to 100
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", number);
+		}
+	}
+	printf("\n");
+	return (0);
+}
