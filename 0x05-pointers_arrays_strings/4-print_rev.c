192ca8afffacd9d53c0d94b9c9a81b5484eb9d8d
Abongwe Wara <wabongwe@gmail.com>
2023-06-28 09:19:10 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string,
+ * in reverse, followed by a new line
+ * @s: characters of string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int n;
+
+	n = 0;
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	while (n >= 0)
+	{
+		_putchar(*(s + n));
+		n--;
+	}
+	_putchar('\n');
+}
