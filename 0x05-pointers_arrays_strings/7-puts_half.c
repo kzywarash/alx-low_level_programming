728e6ee0dbc234773a3d64731d92cfddb09662e5
Abongwe Wara <wabongwe@gmail.com>
2023-06-28 14:55:08 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string, followed by a new line
+ * @str: string to print
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int l;
+	int i;
+
+	l = 0;
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 0)
+	{
+		i = l / 2;
+	}
+	else
+	{
+		i = (l - 1) / 2;
+	}
+	while (i <= l)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
